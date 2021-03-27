506bc822f0750ceec4b077cb0ddd8b3def2d0d91
Kim Greenbush <kim_greenbush@icloud.com>
2021-03-27 15:58:46 -0700

    add methods to export, reusability and readability

--- src/components/PageHome.vue
+++ src/components/PageHome.vue
@@ -3,8 +3,8 @@
     <div v-for = "thread in threads" :key="thread.id">
         <h2>{{thread.title}}</h2>
         <div v-for = "postId in thread.posts" :key="postId">
-            <p>{{users.find( u => u.id === posts.find(p => p.id === postId).userId).username}}</p>
-            <p>{{posts.find(p => p.id === postId).text}}</p>
+            <p>{{userById(postById(postId).userId).username}}</p>
+            <p>{{postById(postId).text}}</p>
         </div>
     </div>
 </template>
@@ -19,6 +19,14 @@ export default {
             posts: sourceData.posts,
             users: sourceData.users,
         }
+    },
+    methods: {
+        postById (postId){
+            return this.posts.find(p => p.id === postId)
+        },
+        userById (userId){
+            return this.users.find( u => u.id === userId)
+        },
     }
 }
 </script>
