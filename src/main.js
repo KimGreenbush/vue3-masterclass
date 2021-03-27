c356f5ad43a8dfe7f112c556e8e5b6731d54d6ba
Kim Greenbush <kim_greenbush@icloud.com>
2021-03-27 12:23:49 -0700

    how to isolate/add app instances and customize

--- src/main.js
+++ src/main.js
@@ -1,4 +1,29 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+// extract app instance to a variable for isolation && customization before mounting (plugins, etc.)
+const firstApp = createApp(App)
+
+// CUSTOMIZATIONS 1
+
+// global components:
+// firstApp.component( name: "aButton", component: {})
+
+//plugins:
+// firstApp.use(aPlugin)
+
+firstApp.mount('#app')
+
+// other isolated app instance for customization
+// const secondApp = createApp(App)
+
+// CUSTOMIZATIONS 2
+
+// global components:
+// secondApp.component( name: "anotherButton", component: {})
+
+//plugins:
+// secondApp.use(anotherPlugin)
+
+                         // other #id where to mount
+// secondApp.mount('#second-app')
