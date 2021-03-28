3a48218f4054ef5ef6eee3d63dd295fc39e119f8
Kim Greenbush <kim_greenbush@icloud.com>
2021-03-28 13:11:54 -0700

    refactored folder org && file names

--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,19 @@
-import PageHome from "@/components/PageHome";
-import PageThreadShow from "@/components/PageThreadShow";
-import PageNotFound from "@/components/PageNotFound";
 import { createRouter, createWebHistory } from "vue-router";
+import Home from "@/pages/Home";
+import ThreadShow from "@/pages/ThreadShow";
+import NotFound from "@/pages/NotFound";
 import sourceData from "@/data.json";
 
 const routes = [
 	{
 		path: "/",
 		name: "Home",
-		component: PageHome
+		component: Home
 	},
 	{
 		path: "/thread/:id",
 		name: "ThreadShow",
-		component: PageThreadShow,
+		component: ThreadShow,
 		props: true,
 		beforeEnter (to, from, next) {
 			// check if thread exists
@@ -36,7 +36,8 @@ const routes = [
 	{
 		path: "/:pathMatch(.*)*",
 		name: "NotFound",
-		component: PageNotFound
+		component:
+	NotFound
 	},
 ];
 
