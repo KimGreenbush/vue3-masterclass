14ec272e2479da5fafb21e23a1914531d39750c8
Kim Greenbush <kim_greenbush@icloud.com>
2021-03-28 17:53:46 -0700

    extract PostEditor, listen/$emit, child -> parent

---
+++ src/components/PostEditor.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="col-full">
+    <form @submit.prevent="save">
+      <div class="form-group">
+        <label for="thread_content">Content:</label>
+        <textarea
+          id="thread_content"
+          class="form-input"
+          name="content"
+          v-model="newPostText"
+        ></textarea>
+      </div>
+
+      <div class="btn-group">
+        <button class="btn btn-ghost">Cancel</button>
+        <button class="btn btn-blue" type="submit" name="Publish">
+          Publish
+        </button>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "PostEditor",
+  data() {
+    return {
+      newPostText: "",
+    };
+  },
+  methods: {
+    save() {
+      const postId = "ggg" + Math.random();
+      const post = {
+        id: postId,
+        text: this.newPostText,
+        publishedAt: Math.floor(Date.now() / 1000),
+        userId: "Miej9zSGMRZKDvMXzfxjVOyv3RF3",
+      };
+      this.$emit("save", { post }); // accessible object with kv pair: eventData.post
+      this.newPostText = "";
+    },
+  },
+};
+</script>
+
+<style scoped>
+</style>
--- src/pages/ThreadShow.vue
+++ src/pages/ThreadShow.vue
@@ -4,36 +4,20 @@
     <h1>{{ thread.title }}</h1>
 
     <PostList :posts="threadPosts" />
-
-    <form @submit.prevent="addPost">
-      <div class="form-group">
-        <label for="thread_content">Content:</label>
-        <textarea
-          id="thread_content"
-          class="form-input"
-          name="content"
-          v-model="newPostText"
-        ></textarea>
-      </div>
-
-      <div class="btn-group">
-        <button class="btn btn-ghost">Cancel</button>
-        <button class="btn btn-blue" type="submit" name="Publish">
-          Publish
-        </button>
-      </div>
-    </form>
+    <PostEditor @save="addPost" />
   </div>
 </template>
 
 <script>
 import sourceData from "@/data.json";
 import PostList from "@/components/PostList";
+import PostEditor from "@/components/PostEditor";
 
 export default {
   name: "ThreadShow",
   components: {
     PostList,
+    PostEditor,
   },
   props: {
     id: {
@@ -45,7 +29,6 @@ export default {
     return {
       threads: sourceData.threads,
       posts: sourceData.posts,
-      newPostText: ""
     };
   },
   computed: {
@@ -54,23 +37,19 @@ export default {
     },
     threadPosts() {
       return this.posts.filter((post) => post.threadId === this.id);
-    }
+    },
   },
   methods: {
-    addPost() {
-      const postId = "ggg" + Math.random()
+    addPost(eventData) {
       const post = {
-        id: postId,
-        text: this.newPostText,
-        publishedAt: Math.floor(Date.now()/1000),
+        ...eventData.post,
         threadId: this.id,
-        userId: "Miej9zSGMRZKDvMXzfxjVOyv3RF3"
-      }
-    this.posts.push(post)
-    this.thread.posts.push(postId)
-    this.newPostText = ""
+      };
+      this.posts.push(post);
+      this.thread.posts.push(post.id);
+      console.log(post);
     },
-  }
+  },
 };
 </script>
 
