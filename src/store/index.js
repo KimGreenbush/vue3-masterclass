b08e26069e878c911124144e68d42798bf6ec337
Kim Greenbush <kim_greenbush@icloud.com>
2021-03-30 20:55:23 -0700

    move user funcs to store state for multi-comp use

--- src/store/index.js
+++ src/store/index.js
@@ -7,22 +7,41 @@ export default createStore({
 		authId: "ALXhxjwgY9PinwNGHpfai6OWyDu2",
 	},
 	getters: {
-		authUser: (state) => state.users.find((user) => user.id === state.authId),
-	},
-	actions: {
-		createPost(context, post) {
-			post.id = "ggg" + Math.random();
-			context.commit("setPost", { post }); //set post
-			context.commit("appendPostToThread", { postId: post.id, threadId: post.threadId }); //append post to thread
+		authUser: (state) => {
+			const user = state.users.find((user) => user.id === state.authId);
+			if (!user) return null
+			return {
+                ...user,
+                // "get" keyword makes the functions accessible as properties
+				get posts() {
+					return state.posts.filter((post) => post.userId === user.id)
+				},
+				get postsCount() {
+					return this.posts.length;
+				},
+				get threads() {
+					return state.threads.filter((thread) => thread.userId === user.id)
+				},
+				get threadsCount() {
+					return this.threads.length
+				},
+			}
 		},
 	},
-	mutations: {
-		setPost(state, { post }) {
-			state.posts.push(post);
-		},
-		appendPostToThread(state, { postId, threadId }) {
-			const thread = state.threads.find((thread) => thread.id === threadId);
-			thread.posts.push(postId);
-		},
-	},
-});
+    actions: {
+        createPost(context, post) {
+            post.id = "ggg" + Math.random()
+            context.commit("setPost", { post }) //set post
+            context.commit("appendPostToThread", { postId: post.id, threadId: post.threadId }) //append post to thread
+        },
+    },
+    mutations: {
+        setPost(state, { post }) {
+            state.posts.push(post)
+        },
+        appendPostToThread(state, { postId, threadId }) {
+            const thread = state.threads.find((thread) => thread.id === threadId)
+            thread.posts.push(postId)
+        },
+    },
+})
