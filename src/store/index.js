de4cfc51e786cbc4df18f4e9d0eb11c0fdd6613d
Kim Greenbush <kim_greenbush@icloud.com>
2021-04-29 19:45:14 -0700

    create ThreadCreate, methods in store

--- src/store/index.js
+++ src/store/index.js
@@ -22,20 +22,30 @@ export default createStore({
 				get threads() {
 					return state.threads.filter((thread) => thread.userId === user.id);
 				},
-				get threadsCount() {
+				get threadsCount(){
 					return this.threads.length;
 				},
 			};
 		},
 	},
 	actions: {
+		// deconstructed {commit} out of
+		//				  (context, payload)
 		createPost({ commit, state }, post) {
 			post.id = "ggg" + Math.random();
 			post.userId = state.authId;
-			(post.publishedAt = Math.floor(Date.now() / 1000)), commit("setPost", { post }); //set post
+			post.publishedAt = Math.floor(Date.now() / 1000);
+			commit("setPost", { post }); //set post
 			commit("appendPostToThread", { postId: post.id, threadId: post.threadId }); //append post to thread
 		},
-		// deconstructed {commit} out of context (ref above method)
+		createThread({ commit, state, dispatch}, { text, title, forumId }) {
+			const id = "ggg" + Math.random();
+			const userId = state.authId;
+			const publishedAt = Math.floor(Date.now() / 1000);
+			const thread = { forumId, publishedAt, title, userId, id };
+			commit("setThread", { thread })
+			dispatch("createPost", { text, threadId: id })
+		},
 		updateUser({ commit }, user) {
 			commit("setUser", { user, userId: user.id });
 		},
@@ -43,6 +53,9 @@ export default createStore({
 	mutations: {
 		setPost(state, { post }) {
 			state.posts.push(post);
+		},
+		setThread(state, { thread }) {
+			state.threads.push(thread);
 		},
 		appendPostToThread(state, { postId, threadId }) {
 			const thread = state.threads.find((thread) => thread.id === threadId);
